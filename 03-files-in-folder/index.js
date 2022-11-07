7e47750d8ebcd00d4577924a188cdc1bdec4a2f7
github-username <72346436+Pashabn@users.noreply.github.com>
2022-11-07 21:14:53 +0300

    fix: fix third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,28 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-const check = (_path) => {
-    fs.readdir(_path, (err, files) => {
-            if (err)
-                console.log(err);
-            else {
-                files.forEach(file => {
-                        fs.stat(path.join(_path, file), (err, s) => {
-                            // s.isFile() && console.log(`${file.replace('.', ' - ')} - ${s.size}b`)
-                            s.isFile() && console.log(
-                                path.basename(file).split('.').slice(0, -1).join('.')
-                                + ' - ' +
-                                path.extname(file).slice(1)
-                                + ' - ' +
-                                s.size
-                                + 'b'
-                            )
-                        });
-                    }
-                )
-            }
-        }
-    )
+const check = async (_path) => {
+
+    const files = await fs.promises.readdir(_path)
+
+    for (const file of files) {
+        fs.stat(path.join(_path, file), (err, s) => {
+            s.isFile() && console.log(
+                path.basename(file, path.extname(file))
+                + ' - ' +
+                path.extname(file).slice(1)
+                + ' - ' +
+                s.size
+                + 'b'
+            )
+        });
+    }
 }
 
 check(path.join(__dirname, 'secret-folder'))
