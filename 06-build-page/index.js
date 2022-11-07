bdb64cc140e7d60125a6ca99de1cced15cf1655b
github-username <72346436+Pashabn@users.noreply.github.com>
2022-11-07 21:54:07 +0300

    refactor: refactor sixth task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,10 +3,6 @@ const path = require('path');
 
 const buildPage = (_path) => {
 
-    const firstStart = async () => {
-        await fs.promises.mkdir(path.join(_path, 'project-dist'), {recursive: true})
-    }
-
     const clear = async () => {
         await fs.promises.rm(path.join(_path, 'project-dist'), {recursive: true, force: true})
     }
@@ -48,29 +44,6 @@ const buildPage = (_path) => {
 
     const readTemp = async (_path) => {
 
-        // const rs = fs.createReadStream(path.join(_path, 'template.html'), {encoding: 'utf-8'})
-        // let template = '';
-        // rs.on('data', (chunk) => {
-        //     template += chunk
-        // });
-        // rs.on('end', () => {
-        //     console.log(template)
-        //
-        //     fs.readdir(path.join(_path, 'components'), (err, files) => {
-        //         if (err) console.log(err);
-        //         files.forEach(async file => {
-        //             let name = path.basename(file, path.extname(file))
-        //             fs.readFile(path.join(_path, 'components', file), {encoding: 'utf-8'},(err, data) => {
-        //                 if (err) throw err;
-        //                 template = template.replace(`{{${name}}}`, data)
-        //                 // console.log(template)
-        //                 // console.log(data)
-        //             } )
-        //         })
-        //         console.log(template)
-        //     })
-        // })
-
         let template = await fs.promises.readFile(path.join(_path, 'template.html'), {encoding: 'utf-8'})
 
         let files = await fs.promises.readdir(path.join(_path, 'components'))
@@ -78,14 +51,13 @@ const buildPage = (_path) => {
         for (const file of files) {
             let name = path.basename(file, path.extname(file))
             const fileBody = await fs.promises.readFile(path.join(_path, 'components', file), {encoding: 'utf-8'})
-                template = template.replace(`{{${name}}}`, fileBody)
+            template = template.replace(`{{${name}}}`, fileBody)
         }
-        await fs.promises.writeFile(path.join(_path,'project-dist', 'index.html'), template, 'utf-8')
+
+        await fs.promises.writeFile(path.join(_path, 'project-dist', 'index.html'), template, 'utf-8')
     }
 
-
-    firstStart()
-        .then(() => clear())
+    clear()
         .then(() => copyDir(_path))
         .then(() => copyStyle(_path))
         .then(() => readTemp(_path))
